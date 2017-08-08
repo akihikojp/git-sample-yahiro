b870269af271b99e5522991332bec48102116302
yahiro <akihiko.yahiro@rakus.co.jp>
2017-08-08 15:25:36 +0900

    走行スピードが遅いので、飛行機能を追加。

--- src/main/java/jp/co/rakus/git_sample_yahiro/Car.java
+++ src/main/java/jp/co/rakus/git_sample_yahiro/Car.java
@@ -9,14 +9,33 @@ package jp.co.rakus.git_sample_yahiro;
 public class Car {
 	/**スピード*/
 	private int speed;
-
+	/**飛ぶ*/
+	private int flyAway;
+	
 	/**
 	 *走る 
 	 */
 	public void run() {
 		this.speed = speed + 5;
 	}
+	
+	/**
+	 *飛ぶ 
+	 */
+	public void flyAway(){
+		this.speed = speed + 50;
+	}
 
+	
+	public int getFlyAway() {
+		return flyAway;
+	}
+
+	public void setFlyAway(int flyAway) {
+		this.flyAway = flyAway;
+	}
+	
+	
 	public int getSpeed() {
 		return speed;
 	}
